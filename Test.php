016cf1de424aa0f2b55304988d206ca1c44068ac
Anarchius <adrijan93@gmail.com>
2019-09-12 21:33:31 +0200

    Test table

---
+++ Test.php
@@ -0,0 +1,21 @@
+<?php>
+    <table>
+        <tr>
+
+            <th></th>
+              <th scope="col">Stage 1</th>
+              <th scope="col">Stage 2</th>
+              <th scope="col">Stage 3</th>
+              <th scope="col">Stage 4</th>
+
+        </tr>
+        <tr>
+
+            <th scope="row">Path</th>
+              <td>Core code</td>
+              <td>Transform</td>
+              <td>Extensions</td>
+              <td>Optimize</td>
+
+        </tr>
+    </table>
